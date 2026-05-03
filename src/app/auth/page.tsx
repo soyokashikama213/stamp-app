640bfb89c368e5728b6b2644b3cf69680049769a
soyokashikama213 <soyoka.shikama213@gmail.com>
2026-05-04 03:16:25 +0900

    Update page.tsx

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -22,7 +22,7 @@ export default function AuthPage() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "https://stamp-app-ashy.vercel.app", // ← ここも修正
+       redirectTo: "https://regoillibgrxphkadnlv.supabase.co/auth/v1/callback"
       },
     });
   };

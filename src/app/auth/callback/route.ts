6483b5c2a1afd40f1012d7111a697a76fbedae34
soyokashikama213 <soyoka.shikama213@gmail.com>
2026-05-04 11:34:36 +0900

    Create route.ts

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,24 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return cookies().get(name)?.value;
+        },
+      },
+    }
+  );
+
+  // セッション取得（ここで確定する）
+  await supabase.auth.getSession();
+
+  return NextResponse.redirect(new URL("/", requestUrl.origin));
+}
